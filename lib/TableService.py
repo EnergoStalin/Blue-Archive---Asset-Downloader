7868051a191b53d5e08b510fb0c9b522398c840c
Alexey Dashko <alex.vipers@mail.ru>
2024-01-15 15:22:29 +0300

    Revert "fix: untested https://github.com/K0lb3/Blue-Archive---Asset-Downloader/issues/5"
    This reverts commit 9fb82ae1a4b96742a15f20182dd3165b52bcad06.

--- lib/TableService.py
+++ lib/TableService.py
@@ -10,7 +10,9 @@ from typing import Union
 class TableZipFile(ZipFile):
     def __init__(self, file: Union[str, BytesIO], name: str = None) -> None:
         super().__init__(file)
-        self.password = b64encode(MersenneTwister(CalculateHash(name if not isinstance(file, str) else os.path.basename(file))).NextBytes(15))
+        self.password = str(
+            CalculateHash(name if not isinstance(file, str) else os.path.basename(file))
+        ).encode()
 
     def open(self, name: str, mode: str = "r", force_zip64=False):
         return super(self.__class__, self).open(
